f25e00410e6e2c6a97f8b576a86aa7d2214c8013
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2024-03-25 14:14:42 +0100

    Update routes.py

--- BACKEND/routes.py
+++ BACKEND/routes.py
@@ -1,6 +1,9 @@
 from flask import request, jsonify
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
-from models import db, User, Contact, TokenBlocklist
+from models import db, User, Contact, TokenBlocklist, Conv, ConvMember
+from Crypto.Hash import SHA256
+import os
+from sqlalchemy import or_, and_
 
 def init_routes(app):
     @app.route('/login', methods=['POST'])
@@ -13,20 +16,87 @@ def init_routes(app):
         password = req_data['password']
         
         # Recherche dans la base de données un utilisateur ayant le nom d'utilisateur et le mot de passe fournis
-        user = User.query.filter_by(username=username, password_hash=password).first()
-        
-        # Vérifie si un utilisateur correspondant aux informations fournies a été trouvé dans la base de données
+       user = User.query.filter_by(username=username).first()
         if user:
-            # Affiche un message dans la console indiquant que l'utilisateur a été authentifié avec succès
-            print("User %s authenticated" % username)
-            # Crée un token d'accès JWT (JSON Web Token) pour cet utilisateur. L'identité du token est définie sur l'ID de l'utilisateur dans la base de données
-            access_token = create_access_token(identity=user.idUser)
-            # Renvoie le token d'accès JWT dans une réponse JSON avec le code d'état HTTP 200 (OK)
-            return jsonify({'access_token': access_token, 'idUser':user.idUser}), 200
+            stored_salt = bytes.fromhex(user.salt) #On converti la chaine de caractère en byte
+            stored_password_hash = user.password_hash
+            #on compare le hash du mdp rentré et le mdp hashé dans la bdd
+            hashed_password=hashPassword(password,stored_salt)
+            if(stored_password_hash==hashed_password):
+                # Crée un token d'accès JWT (JSON Web Token) pour cet utilisateur. L'identité du token est définie sur l'ID de l'utilisateur dans la base de données
+                access_token = create_access_token(identity=user.idUser)
+                # Renvoie le token d'accès JWT dans une réponse JSON avec le code d'état HTTP 200 (OK)
+                return jsonify({'access_token': access_token, 'idUser':user.idUser}), 200
         
         # Si l'utilisateur n'est pas trouvé dans la base de données ou si les informations d'identification sont incorrectes, renvoie un message d'erreur JSON avec le code d'état HTTP 401 (Unauthorized)
         return jsonify({'message': 'Invalid credentials'}), 401
+    
+    @app.route('/signUp', methods=['POST'])
+    def signUp():
+        req_data = request.get_json()
+        username = req_data['username']
+        password = req_data['password']
+        email= req_data["email"]
+        
+        #Salage et hachage du mdp
+        salt = os.urandom(32)
+        hashed_password = hashPassword(password, salt)
+        
+        new_user = User(username=username, email=email, password_hash=hashed_password, salt=salt.hex())
+        db.session.add(new_user)
+        db.session.commit()
+        user_id = new_user.idUser
+        if new_user:
+            access_token = create_access_token(identity=user_id)
+            return jsonify({'access_token': access_token}), 200
+        return jsonify({'message': 'Invalid credentials'}), 401
 
+
+    def hashPassword(password, salt):
+        # Ajouter le sel au mot de passe
+        salted_password = password.encode() + salt
+
+        # Hacher le mot de passe avec le sel en utilisant SHA-256
+        hash_object = SHA256.new(data=salted_password)
+        hashed_password = hash_object.hexdigest()
+        return hashed_password
+
+  
+    @app.route('/displayUserByName', methods=['POST'])
+    @jwt_required()
+    def displayUserByName():
+        req_data = request.get_json()
+        username = req_data['username']
+        id_user = get_jwt_identity()
+        
+        users = User.query.filter(
+        and_(
+            ~User.idUser.in_(db.session.query(Contact.id_contact).filter_by(id_user=id_user)),
+            User.username.like(f'%{username}%'),
+            User.idUser != id_user
+        )).limit(100)
+        
+        user_data = [{'id': user.idUser, 'username': user.username} for user in users]
+        return jsonify({'users': user_data}), 200
+
+    
+    @app.route('/addFriend', methods=['POST'])
+    @jwt_required()
+    def addFriend():
+        req_data = request.get_json()
+        username = req_data['username']
+        id_contact= req_data['id_contact']
+        id_user = get_jwt_identity()
+        
+        new_contact = Contact(id_user=id_user, id_contact=id_contact)
+        db.session.add(new_contact)
+        db.session.commit()
+        
+        if new_contact:
+            access_token = create_access_token(identity=id_user)
+            return jsonify({'message': 'Contact added successfully', 'access_token': access_token}), 200
+        else:
+            return jsonify({'message': 'Error adding contact'}), 500
 
     # Route protégée nécessitant un token JWT valide
     @app.route('/protected', methods=['GET'])
