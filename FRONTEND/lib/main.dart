98a6aab156fed6ae440e4a7ab8b85b1313868af6
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2024-03-25 17:23:02 +0100

    Update main.dart

--- FRONTEND/lib/main.dart
+++ FRONTEND/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(), // CALL LOGIN PAGE -> ui/screens/loginpage.dart
+      home: AuthenticationScreen(), // CALL LOGIN PAGE -> ui/screens/loginpage.dart
     );
   }
 }
