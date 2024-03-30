954b66f46a6ad3a5e10154c64d6f627cc160fb34
JulianGssl <120329475+JulianGssl@users.noreply.github.com>
2024-03-30 17:58:24 +0100

    Update config.py

--- BACKEND/config.py
+++ BACKEND/config.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = secrets.token_hex(32)
     JWT_SECRET_KEY = secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Cytech0001$@localhost/chatApp'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Cytech0001$@localhost/chatApp' # Modifier le mdp et le nom de la bdd accordement
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     SESSION_COOKIE_NAME = 'custom_session_cookie'
