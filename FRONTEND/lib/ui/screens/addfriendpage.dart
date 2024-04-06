4be6f22dcea91369e178b40ea6fb4840d9854446
JulianGssl <120329475+JulianGssl@users.noreply.github.com>
2024-04-06 22:03:45 +0200

    Update addfriendpage.dart

--- FRONTEND/lib/ui/screens/addfriendpage.dart
+++ FRONTEND/lib/ui/screens/addfriendpage.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 import '../../models/url.dart';
+import '../../models/constants.dart';
+
+import 'customebottomnavbar.dart';
 import '../../models/user.dart';
+import 'chatlistpage.dart';
 import 'contactpage.dart';
+
 
 class AddFriendPage extends StatefulWidget {
   final String sessionToken;
   final User currentUser;
 
-  const AddFriendPage({super.key, required this.sessionToken,  required this.currentUser});
+  const AddFriendPage({super.key, required this.sessionToken, required this.currentUser});
 
   @override
   _AddFriendPageState createState() => _AddFriendPageState();
@@ -22,36 +28,32 @@ class _AddFriendPageState extends State<AddFriendPage> {
   TextEditingController searchController = TextEditingController();
   late String _csrfToken;
 
-
   @override
   void initState() {
     _fetchCSRFToken('get_CSRF/displayUserByName/2');
     super.initState();
   }
 
-
-  
-
-Future<String> _fetchCSRFToken(String formRoute) async {
+  Future<String> _fetchCSRFToken(String formRoute) async {
   // FormRoute représente la route spécifique pour obtenir le jeton CSRF
-  final response = await http.get(
-    Uri.parse('$url/$formRoute'), // Utilisez la route spécifique passée en paramètre
-    headers: {
-      'Authorization': 'Bearer ${widget.sessionToken}',
-      'Content-Type': 'application/json'
-    },
-  );
-  if (response.statusCode == 200) {
-    var responseData = json.decode(response.body);
-    String csrfToken = responseData['csrf_token'];
-    setState(() {
-      _csrfToken = csrfToken;
-    });
-    return response.body;
-  } else {
-    throw Exception('Failed to load CSRF token');
+    final response = await http.get(
+      Uri.parse('$url/$formRoute'), // Utilisez la route spécifique passée en paramètre
+      headers: {
+        'Authorization': 'Bearer ${widget.sessionToken}',
+        'Content-Type': 'application/json'
+      },
+    );
+    if (response.statusCode == 200) {
+      var responseData = json.decode(response.body);
+      String csrfToken = responseData['csrf_token'];
+      setState(() {
+        _csrfToken = csrfToken;
+      });
+      return response.body;
+    } else {
+      throw Exception('Failed to load CSRF token');
+    }
   }
-}
 
 
   // Fonction pour récupérer les utilisateurs depuis le backend Flask
@@ -105,49 +107,127 @@ Future<String> _fetchCSRFToken(String formRoute) async {
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ajouter des amis'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.center,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFF1C0F45),
+            Color(0xFF6632C6),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: 'Rechercher un utilisateur',
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+         appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => ChatListPage(
+                  currentUser: widget.currentUser,
+                  sessionToken: widget.sessionToken,
+                ),
+              ));
+            },
+          ),
+          title: Text('Add Friend', style: TextStyle(fontFamily: 'fontLufga', color: Colors.white)),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: searchController,
+                style: TextStyle(color: Colors.white, fontFamily: 'fontLufga'),
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  hintText: 'Rechercher un utilisateur',
+                  hintStyle: TextStyle(fontFamily: 'fontLufga', color: Colors.white60),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              fetchUsers(searchController.text);
-            },
-            child: Text('Rechercher'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(users[index]['username'] ?? ''),
-                  trailing: ElevatedButton(
-                    onPressed: () {
-                      // Appeler une fonction pour gérer l'ajout de l'utilisateur ici
-                      addUser(users[index]['username'] ?? '',
-                          users[index]["id"] ?? '');
-                    },
-                    child: Text('Ajouter'),
-                  ),
-                );
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                fetchUsers(searchController.text);
               },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 80.0),
+                elevation: 5.0,
+                shadowColor: Colors.black54,
+              ),
+              child: Text(
+                'Rechercher',
+                style: TextStyle(fontFamily: 'fontLufga', color: Colors.white),
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Stack(
+                    alignment: Alignment.centerRight,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 100.0), // donnez de l'espace pour le bouton
+                        child: ListTile(
+                          title: Text(
+                            users[index]['username'] ?? '',
+                            style: TextStyle(fontFamily: 'fontLufga', color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 16.0, // pour positionner le bouton à droite
+                        child: ElevatedButton(
+                          onPressed: () {
+                            addUser(users[index]['username'] ?? '', users[index]["id"] ?? '');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple, // Couleur de fond
+                            foregroundColor: Colors.white, // Couleur du texte
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0), // Bords arrondis
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 32.0), // Padding ajusté pour le bouton
+                            elevation: 5.0, // Élévation pour l'effet d'ombre
+                            shadowColor: Colors.black54, // Couleur de l'ombre
+                          ),
+                          child: Text(
+                            'Ajouter',
+                            style: TextStyle(fontFamily: fontLufga), // Style du texte sans spécifier la taille
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: CustomBottomAppBar(currentUser: widget.currentUser, sessionToken: widget.sessionToken, activeIndex: 2,),
       ),
     );
   }
+
+
 }
